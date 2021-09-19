8bccd93a21f0b54352cad9e47f6428b43f77b6b0
DingLe <zabbix2022@163.com>
2021-09-19 23:24:37 +0800

    ssh commit

--- src/main/java/com/atguigu/github/GitTest.java
+++ src/main/java/com/atguigu/github/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("hot-fix2");
+        System.out.println("ssh");
     }
 }
 
