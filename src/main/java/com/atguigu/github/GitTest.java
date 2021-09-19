56832aa4c15848a606f8521302b43427e48a1336
github2027 <90952853+github2027@users.noreply.github.com>
2021-09-19 23:28:25 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/github/GitTest.java
+++ src/main/java/com/atguigu/github/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix2");
         System.out.println("ssh");
+        System.out.println("pull");
     }
 }
 
